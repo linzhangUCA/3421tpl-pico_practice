05878459487ef1568da6682a3008044e7953e5de
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2025-09-02 21:39:22 -0500

    Update switch_mode.py

--- switch_mode.py
+++ switch_mode.py
@@ -1,12 +1,4 @@
-"""
-Switch LED mode
-
-Mode 1: fade in fade out
-Mode 2: constantly on
-Press button to toggle the mode
-"""
-
-from machine import Pin, PWM
+from machine import Pin, PWM, Timer
 from time import sleep
 
 # SETUP
