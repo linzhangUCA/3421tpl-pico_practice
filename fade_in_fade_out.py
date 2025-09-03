e1e94ce5a4167157da6042768a431cb80ff326d1
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2025-09-02 21:39:02 -0500

    Update fade_in_fade_out.py

--- fade_in_fade_out.py
+++ fade_in_fade_out.py
@@ -1,12 +1,3 @@
-"""
-Make a dimmer LED.
-Repeatedly fade in and fade out an LED.
-Use 2 second to fade in and 1 second to fade out.
-
-Hint: PWM
-"""
-
-
 from machine import Pin, PWM, Timer
 from time import sleep
 
